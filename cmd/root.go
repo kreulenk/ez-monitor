2a33354b0985368c8e640d5ec138c6b72f082a63
Kevin Kreulen <16885904+kreulenk@users.noreply.github.com>
2025-04-16 18:52:52 -0500

    Add --version flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,36 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"github.com/kreulenk/ez-monitor/internal/build"
 	"github.com/kreulenk/ez-monitor/pkg/inventory"
 	"github.com/kreulenk/ez-monitor/pkg/statistics"
 	"github.com/kreulenk/ez-monitor/pkg/tui"
 	"github.com/spf13/cobra"
 	"os"
+	"runtime"
 )
 
 func genRootCmd() *cobra.Command {
+	var version bool
 
 	var cmd = &cobra.Command{
 		Use:   "ez-monitor <inventory-file>",
 		Short: "An agentless SSH based system monitoring tool",
 		Long:  `EZ-Monitor allows you to easily monitor your Linux infrastructure by only requiring SSH connections`,
-		Args:  cobra.ExactArgs(1),
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 && !version {
+				return errors.New("no inventory file was provided")
+			}
+			return nil
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			ctx := cmd.Context()
+			if version {
+				fmt.Printf("%s\n%s_%s\n%s\n", build.Version, runtime.GOOS, runtime.GOARCH, build.SHA)
+				return
+			}
 
 			filename := args[0]
 			inventoryInfo, err := inventory.LoadInventory(filename)
@@ -27,6 +41,8 @@ func genRootCmd() *cobra.Command {
 			tui.Initialize(ctx, inventoryInfo, statsChan)
 		},
 	}
+	cmd.Flags().BoolVarP(&version, "version", "v", false, "Show version information")
+
 	return cmd
 }
 
---
+++ internal/build/build.go
@@ -0,0 +1,8 @@
+// The build package is used by the --version command. Actual Version and SHA information is injected during the release
+// build pipeline
+
+package build
+
+var Version = "manualBuild"
+
+var SHA = "manualBuild"
